e8ab7c1f81f9ede086658a8d87a44c9e223f0b75
kim2ju <bamtori05@naver.com>
2024-02-01 00:28:47 +0900

    feat: spring boot 번역 서버 추가

---
+++ translate-server/src/main/java/com/yonseiaws5/gamehelper/GameHelperApplication.java
@@ -0,0 +1,13 @@
+package com.yonseiaws5.gamehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameHelperApplication.class, args);
+	}
+
+}
